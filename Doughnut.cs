a394b851ee9cfa8de5ca42b15de8fa91281b66b9
nemesbianca <missbianca1999@yahoo.com>
2020-11-28 16:58:43 +0200

    Am adaugat clasa Doughnut si am creat toate evenimentele in MainWindow.xaml.cs

---
+++ Doughnut.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nemes_Bianca_Lab2
+{
+    class Doughnut
+    {
+        private DoughnutType mFlavor;
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private float mPrice;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mFlavor = aFlavor;
+        }
+    }
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+}
